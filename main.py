9d4aa596fb3b4dd173388eb70707ddcd803e38f1
ioanzicu <ioan.zicu94@gmail.com>
2022-02-13 05:34:53 +0000

    Init commit

---
+++ main.py
@@ -0,0 +1,190 @@
+import json
+from typing import Any
+import pprint
+from requests import get
+from bs4 import BeautifulSoup
+from httplib2 import Response
+from collections import Counter
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis.network import Network
+
+
+pp = pprint.PrettyPrinter(depth=4, indent=2)
+
+
+class ArgumentNotProvided(ValueError):
+    pass
+
+
+class WebpageParser():
+    def __init__(self, root_link: str) -> None:
+        if not isinstance(root_link, str):
+            raise ValueError(
+                f'root_link is of type {root_link}, expected to be str')
+
+        self.root_link: str = root_link
+        self.root_host: str = root_link.split('.')[1] if self.root_link else ''
+        self.map_dict: dict = {}
+        self.graph_dict: dict = {}
+
+    def __str__(self) -> str:
+        return f'WebpageParser(root_link={self.root_link})'
+
+    def perform_get_request(self, url: str = ''):
+        # print('-----------', url)
+        if not url:
+            raise ArgumentNotProvided('url was not provided')
+        response: Response = get(url=url)
+
+        if response.status_code != 200:
+            raise Exception(f'Status code is not 200: {response.status_code}')
+        return response.text
+
+    def get_links_from_web_page(self, web_page, parser='html.parser'):
+        soup: BeautifulSoup = BeautifulSoup(web_page, parser)
+        return soup.find_all('a')
+
+    def extract_hrefs(self, links: list, root: str = '') -> dict:
+        if not isinstance(links, list):
+            raise ValueError(
+                f'The links type is {type(links)}, expected to be of type list')
+
+        if not links:
+            return {'internal_links': Counter(), 'external_links': Counter(), 'dead_links': Counter(), 'phone_links': Counter(), 'email_links': Counter()}
+
+        if not root:
+            raise ValueError(
+                f'The root was not provided: links={links}, root={root}')
+
+        internal_links = []
+        external_links = []
+        dead_links = []
+        phone_links = []
+        email_links = []
+        for link in links:
+            link_host: str = link.get('href')
+
+            # print(link_host, link_host.split('.'))
+
+            if link_host.startswith('javascript:;'):
+                continue
+            elif link_host.startswith('/'):
+                if root.endswith('/'):
+                    root = root[:-1]
+                complete_link = root + link_host
+                internal_links.append(complete_link)
+            elif link_host == '#':
+                dead_links.append(link_host)
+            elif 'tel' in link_host:
+                phone_links.append(link_host)
+            elif 'mailto' in link_host:
+                email_links.append(link_host)
+            # check only if the hosts are the same
+            elif len(link_host.split('.')) > 1 and self.root_host in link_host.split('.')[1]:
+                internal_links.append(link_host)
+            else:
+                external_links.append(link_host)
+
+        return {'internal_links':   Counter(internal_links),
+                'external_links':   Counter(external_links),
+                'dead_links':       Counter(dead_links),
+                'phone_links':      Counter(phone_links),
+                'email_links':      Counter(email_links)}
+
+    def extract_links_from_counter(self, counter_obj: Counter) -> list:
+        if not isinstance(counter_obj, Counter):
+            raise ValueError(
+                f'Argument obj is of type {type(counter_obj)}, expected obj to be of type Counter')
+        return [link for link, _ in counter_obj.items()]
+
+    def build_dict_map(self) -> dict:
+        # Perform get request
+        response = self.perform_get_request(url=self.root_link)
+        # Extract links from html root page
+        links = self.get_links_from_web_page(web_page=response)
+        # Categorize links
+        clean_links = self.extract_hrefs(links=links, root=self.root_link)
+        # Extract internal links
+        only_internal_links = self.extract_links_from_counter(
+            clean_links['internal_links'])
+
+        # Add to the map_dict the first key value
+        self.map_dict = {self.root_link: clean_links}
+
+        # Repeat the above steps for the local
+        for internal_link in only_internal_links:
+            response = self.perform_get_request(url=internal_link)
+            page_links = self.get_links_from_web_page(web_page=response)
+            counter_links = self.extract_hrefs(
+                links=page_links, root=internal_link)
+            self.map_dict[internal_link] = counter_links
+        return self.map_dict
+
+    def convert_counters_to_graph_edges(self) -> dict:
+
+        if not self.map_dict:
+            self.build_dict_map()
+
+        for key_root, value_dict in self.map_dict.items():
+            self.graph_dict[key_root] = [(key_root, destination_link, weight)
+                                         for destination_link, weight in value_dict['internal_links'].items()]
+        return self.graph_dict
+
+    def write_graph_dict_to_json_file(self, file_name: str = 'graph_dict') -> None:
+
+        if not self.graph_dict:
+            raise ValueError('The graph_dict is empty')
+
+        with open(f'{file_name}.json', 'w') as fhandle:
+            try:
+                json.dump(self.graph_dict, fhandle, indent=4)
+            except Exception as exc:
+                print(
+                    f'Exception occured when trying to write to json file with name={file_name}: {exc}')
+
+    def load_graph_dict_from_json(self, file_name: str) -> None:
+
+        with open(f'{file_name}.json', 'r') as fhandle:
+            try:
+                self.graph_dict = json.loads(fhandle.read())
+            except Exception as exc:
+                print(
+                    f'Exception occured when trying to read from the json file with name={file_name}: {exc}')
+
+        return self.graph_dict
+
+
+if __name__ == '__main__':
+    root: str = 'https://www.globalapptesting.com'
+
+    webparser = WebpageParser(root_link=root)
+    # response = webparser.perform_get_request(url=root)
+    # links = webparser.get_links_from_web_page(web_page=response)
+    # hrefs = webparser.extract_hrefs(links=links, root=root)
+    # webparser.build_dict_map()
+    # graph_dict = webparser.convert_counters_to_graph_edges()
+
+    # webparser.write_graph_dict_to_json_file()
+    loaded_graph_dict = webparser.load_graph_dict_from_json('graph_dict')
+
+    # VISUALISATION
+    plt.rcParams.update({'font.size': 5})
+    G = nx.Graph()
+    nt = Network(height='1000px', width='100%', directed=True)
+    nx_graph = nx.Graph()
+    nx_graph.add_node(root)
+    for key_root, value_edges in loaded_graph_dict.items():
+        edges_len = len(value_edges) if len(value_edges) > 20 else 20
+        nx_graph.add_node(key_root, label='', width=20,
+                          title=key_root,  size=0.8 * edges_len, group=key_root)
+
+        nx_graph.add_weighted_edges_from([(src, dest, weight*5)
+                                          for (src, dest, weight) in value_edges], arrowStrikethrough=True)
+
+    nx.draw_shell(G, with_labels=False, font_size=4, font_weight='bold')
+    nt.from_nx(nx_graph)
+    nt.show_buttons(filter_=['nodes', 'edges', 'physics'])
+    nt.barnes_hut(gravity=-40000, central_gravity=0, spring_length=400,
+                  spring_strength=0.001, damping=0.49, overlap=0)
+    nt.show(f'{root.split(".")[1]}_map.html')
